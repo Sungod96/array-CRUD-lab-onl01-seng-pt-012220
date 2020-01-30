28134548e3922f03a0cb13aa6db987a7cfef1b18
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-01-30 02:17:39 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
-end
+[]
 
 def create_an_array
-  
+  types_of_pizza = ["sausage","pepperoni","cheese"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  types_of_pizza = ["sausage", "pepperoni", "cheese"]
+  types_of_pizza << "pineapple"
+  puts types_of_pizza.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  types_of_pizza = ["sausage","pepperoni","cheese"]
+  types_of_pizza.unshift
+  puts types_of_pizza.inspect
 end
 
 def remove_element_from_end_of_array(array)
-  
+  types_of_pizza = ["sausage","pepperoni","cheese"]
+  types_of_pizza.pop
+  types_of_pizza.inspect
 end
 
 def remove_element_from_start_of_array(array)
-  
-end
+  types_of_pizza = ["pepperoni","sausage", "cheese"]
+  "pepperoni" = types_of_pizza.shift
+  types_of_pizza.inspect
+  end
 
 def retrieve_element_from_index(array, index_number)
-  
+  types_of_pizza =["pepperoni", "sausage", "cheese", ]
+  types_of_pizza.index ("pepperoni")
 end
 
 def retrieve_first_element_from_array(array)
-  
+  types_of_pizza = ["pepperoni", "sausage", "cheese",]
+  types_of_pizza.index("pepperoni")
 end
 
 def retrieve_last_element_from_array(array)
-  
+  types_of_pizza = ["pepperoni", "sausage" , "cheese"]
+  types_of_pizza.index("cheese")
 end
